6cb13555f10159f75903a27a79055b06505f04ac
Thomas Goettlicher <tgoettlicher@suse.de>
2011-09-20 11:30:00 +0200

    fixed license

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,26 @@
+/* 
+ * Copyright (c) 2010-2011 SUSE LINUX Products GmbH.
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation either version 2 of the 
+ * license or version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact SUSE LINUX Products GmbH.
+ *
+ * To contact SUSE about this file by physical or electronic mail,
+ * you may find current contact information at www.suse.com 
+ *
+ */
+
 #define WEBYAST_UP_ICON		"qwebyast-enabled.png"
 #define WEBYAST_DOWN_ICON	"qwebyast-disabled.png"
 #define WEBYAST_ICON_PATH	"/usr/share/icons/hicolor/32x32/apps/qwebyast/pics/"
--- src/yast.h
+++ src/yast.h
@@ -1,3 +1,27 @@
+/* 
+ * Copyright (c) 2010-2011 SUSE LINUX Products GmbH.
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation either version 2 of the 
+ * license or version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact SUSE LINUX Products GmbH.
+ *
+ * To contact SUSE about this file by physical or electronic mail,
+ * you may find current contact information at www.suse.com 
+ *
+ */
+
+
 #ifndef YAST_H
 #define YAST_H
 
